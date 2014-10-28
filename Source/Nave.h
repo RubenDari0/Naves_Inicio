0c56940f2dcf84785d232fd17a77740479a666be
RubenDari0 <dhark09@hotmail.com>
2014-10-28 07:13:55 -0700

    agregando nave
    definiendo las coordenadas

---
+++ Source/Nave.h
@@ -0,0 +1,24 @@
+#ifndef __NAVE_H__
+#define __NAVE_H__
+#include "Sprite.h"
+
+class Nave{
+	Sprite * sprite;
+	int x;
+	int y;
+	int w;
+	int h;
+	bool autoMovimiento;
+public:
+	void Mover(int posicion);
+	Nave(SDL_Surface * screen, char * rutaImagen, int x, int y);//Constructor
+	void SetAutoMovimiento(bool autoMovimiento);
+	void Pintar();
+	void Actualizar();
+	int ObtenerX();
+	int ObtenerY();
+	int ObtenerW();
+	int ObtenerH();
+};
+
+#endif
